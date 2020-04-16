e6807f44dda7328d675144882cda6bcebb201035
goldy <goldysuryan87@gmail.com>
2020-04-17 03:32:32 +0530

    added another method

--- capitalize/capitalize.js
+++ capitalize/capitalize.js
@@ -6,4 +6,16 @@ function capitalize(str) {
     }).join(' ');
 }
 
-console.log(capitalize('goldy suryan, just do your work'));
+// checking for spaces in string
+function capitalize(str) {
+    let result = str[0].toUpperCase();
+
+    for (let i = 1; i < str.length; i++) {
+        if (str[i - 1] == ' ') {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i]
+        }
+    }
+    return result;
+}
